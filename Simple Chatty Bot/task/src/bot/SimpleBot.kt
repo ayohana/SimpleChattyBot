c7fe54c94cd3dc0b129789aec9a418cbaae22314
Adela Darmansyah <adela.yohana@gmail.com>
2020-07-21 15:01:26 -0700

    Create a function that prints bot name and birth year

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,5 +1,11 @@
 package bot
 
 fun main() {
-    // write your code here
+    printMessage("Aid", 2020)
 }
+
+fun printMessage(botName: String, birthYear: Int) {
+    println("Hello! My name is $botName.")
+    println("I was created in $birthYear.")
+}
+
