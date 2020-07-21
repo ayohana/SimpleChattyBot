5c3dfd1a50f4513097d95d10b96e86ed5248300a
Adela Darmansyah <adela.yohana@gmail.com>
2020-07-21 16:03:14 -0700

    Reads console input

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -1,11 +1,23 @@
 package bot
 
+import java.util.*
+
+
 fun main() {
-    printMessage("Aid", 2020)
-}
+    val scanner = Scanner(System.`in`)
 
-fun printMessage(botName: String, birthYear: Int) {
-    println("Hello! My name is $botName.")
-    println("I was created in $birthYear.")
-}
+    println("Hello! My name is Aid.")
+    println("I was created in 2020.")
+    println("Please, remind me your name.")
 
+    val yourName = readLine()
+
+    println("What a great name you have, $yourName!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    val (remainder3, remainder5, remainder7) = readLine()!!.split(' ')
+    val yourAge = (remainder3.toInt() * 70 + remainder5.toInt() * 21 + remainder7.toInt() * 15) % 105
+
+    println("Your age is $yourAge; that's a good time to start programming!")
+}
